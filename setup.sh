359b585fe0d8e5cf9ebcf8c095cb1a51ecedd92d
Tony <me@tonybenoy.com>
2023-06-10 13:46:21 +0300

    Add poetry

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 files=(".zshrc" ".zshenv" ".bashrc" ".bashrc.aliases" ".p10k.zsh" ".gitconfig")
-for file in * .[^.]*; do 
+for file in * .[^.]*; do
 	if [[ " ${files[*]} " =~ " ${file} " ]]; then
 		echo "Symlinking file $PWD/$file"
-		ln -nfs $PWD/$file /home/$USER/$file 
+		ln -nfs $PWD/$file /home/$USER/$file
 	fi
 done
+ln -nfs $PWD/.config/pypoetry/config.toml /home/$USER/.config/pypoetry/config.toml
